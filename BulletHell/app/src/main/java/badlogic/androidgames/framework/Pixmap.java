c6a5ee40867f74f0e16f17d4d0128425b8a48eca
Hologuardian <namaeskram@gmail.com>
2016-04-19 15:05:18 -0400

    Ripped mr nom for rendering and input.

---
+++ BulletHell/app/src/main/java/badlogic/androidgames/framework/Pixmap.java
@@ -0,0 +1,13 @@
+package badlogic.androidgames.framework;
+
+import badlogic.androidgames.framework.Graphics.PixmapFormat;
+
+public interface Pixmap {
+    public int getWidth();
+
+    public int getHeight();
+
+    public PixmapFormat getFormat();
+
+    public void dispose();
+}
